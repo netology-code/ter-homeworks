4af270cbc17b846072a8c26af11c8022cfa0cf98
Eugene M <70715492+udjin10@users.noreply.github.com>
2023-07-19 13:31:51 +0300

    Update main.tf

--- 01/src/main.tf
+++ 01/src/main.tf
@@ -28,7 +28,7 @@ resource "docker_image" {
 
 resource "docker_container" "1nginx" {
   image = docker_image.nginx.image_id
-  name  = "example_${random_password.random_string_fake.resuld}"
+  name  = "example_${random_password.random_string_FAKE.resulT}"
 
   ports {
     internal = 80
