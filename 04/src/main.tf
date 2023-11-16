95593fec35c61f9c7def178e67c19c209ab7e55a
Ivan Malyshev <ivanmalyshev1994@gmail.com>
2023-11-17 00:18:47 +0300

    add step 1

--- 04/src/main.tf
+++ 04/src/main.tf
@@ -8,3 +8,30 @@ resource "yandex_vpc_subnet" "develop" {
   v4_cidr_blocks = var.default_cidr
 }
 
+module "test-vm" {
+  source          = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
+  env_name        = "develop"
+  network_id      = yandex_vpc_network.develop.id
+  subnet_zones    = ["ru-central1-a"]
+  subnet_ids      = [ yandex_vpc_subnet.develop.id ]
+  instance_name   = "web"
+  instance_count  = 2
+  image_family    = "ubuntu-2004-lts"
+  public_ip       = true
+
+  metadata = {
+      user-data          = data.template_file.cloudinit.rendered
+      serial-port-enable = var.vms_ssh.serial-port-enable
+  }
+
+}
+
+#Пример передачи cloud-config в ВМ для демонстрации №3
+data "template_file" "cloudinit" {
+  template = file("./cloud-init.yml")
+
+  vars = {
+    username           = var.vms_ssh.user
+    ssh_public_key     = file(var.vms_ssh.pub_key)
+  }
+}
--- 04/src/variables.tf
+++ 04/src/variables.tf
@@ -53,5 +53,18 @@ variable "vm_db_name" {
   description = "example vm_db_ prefix"
 }
 
+variable "vms_ssh" {
+
+  default = {
+    user               = "ubuntu"
+    serial-port-enable = 1
+    pub_key            = "~/.ssh/id_ed25519.pub"
+  }
+  type = object({
+    user               = string
+    serial-port-enable = number
+    pub_key            = string
+  })
+}
 
 
