f9317c91af658e5adc5215bc69d153ccdbeeae15
Eugene M <udjin10@gmail.com>
2024-01-09 15:41:44 +0300

    add demo

---
+++ 01/lecture/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {}
+  required_version = ">=0.13"
+}
+
+resource "random_password" "any_uniq_name" {
+  length = 16
+}
+
+resource "local_file" "from_resourse" {
+  content  = random_password.any_uniq_name.result
+  filename = "/tmp/from_resource.txt"
+}
+
+data "local_file" "version" {
+  filename = "/proc/version"
+}
+
+resource "local_file" "from_dataresourse" {
+  content  = data.local_file.version.content
+  filename = "/tmp/from_data_source.txt"
+}
