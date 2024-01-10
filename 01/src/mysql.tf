e5b524b198aab9ed043a60113438b01f1517a909
dimka <ddd@ddd>
2024-01-10 14:46:50 +0300

    create mysql.tf

--- 01/src/mysql.tf
+++ 01/src/mysql.tf
@@ -29,7 +29,9 @@ resource "random_password" "MYSQL_PASSWORD" {
 resource "docker_image" "mysql" {
   name         = "mysql:8"
   keep_locally = true
-  context = "tcp://158.160.25.132:2375"
+  build {
+    context = "context"
+  }
 }
 
 resource "docker_container" "mysql_container" {
