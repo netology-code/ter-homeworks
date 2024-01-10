f22727a6bbf48ae2928d6ad3bb5c81506421fd8e
dimka <ddd@ddd>
2024-01-10 12:25:20 +0300

    change version docker

--- 01/src/main.tf
+++ 01/src/main.tf
@@ -2,8 +2,7 @@ terraform {
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
-      version = "3.0.2"
-      #version = "~> 3.0.1"
+      version = "~> 3.0.1"
     }
   }
   required_version = ">=1.5.1" /*Многострочный комментарий.
