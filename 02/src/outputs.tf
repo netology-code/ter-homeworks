3df879098708f40d4c0b52b45fd60bbd8f8f60f2
dimka <ddd@ddd>
2024-01-13 18:38:37 +0300

    update vars

--- 02/src/outputs.tf
+++ 02/src/outputs.tf
@@ -1,8 +1,8 @@
 output "test" {
 
   value = [
-    { WEB = ["external_ip${yandex_compute_instance.platform.network_interface[0].nat_ip_address}"] },
-    { DB = ["external_ip${yandex_compute_instance.platform-db.network_interface[0].nat_ip_address}"] },
+    { WEB = ["external_ip ${yandex_compute_instance.platform.network_interface[0].nat_ip_address}", "instance_name ${yandex_compute_instance.platform.name}", ""] },
+    { DB = ["external_ip ${yandex_compute_instance.platform-db.network_interface[0].nat_ip_address}"] },
 
   ]
 }
