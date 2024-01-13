b1ced65774fb81e9b004cd8e86c58d2e36a4c71a
dimka <ddd@ddd>
2024-01-13 19:18:55 +0300

    update vars

--- 02/src/variables.tf
+++ 02/src/variables.tf
@@ -4,6 +4,18 @@
   #description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
 #}
 
+variable "cloud_id" {
+  type        = string
+  default     = ""
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
+}
+
+variable "folder_id" {
+  type        = string
+  default     = ""
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
+}
+
 variable "default_zone" {
   type        = string
   default     = "ru-central1-a"
