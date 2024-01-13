439fb91be4e41894b8ff3c224857f5a55aab5a50
dimka <ddd@ddd>
2024-01-13 14:42:42 +0300

    fix

--- 02/src/main.tf
+++ 02/src/main.tf
@@ -14,20 +14,26 @@ data "yandex_compute_image" "ubuntu" {
 }
 resource "yandex_compute_instance" "platform" {
   name        = "netology-develop-platform-web"
-  platform_id = "standard-v4"
+  platform_id = "standard-v1"
+
+  zone = "ru-central1-a"
+
   resources {
     cores         = 1
     memory        = 1
     core_fraction = 5
   }
+
   boot_disk {
     initialize_params {
       image_id = data.yandex_compute_image.ubuntu.image_id
     }
   }
+
   scheduling_policy {
     preemptible = true
   }
+
   network_interface {
     subnet_id = yandex_vpc_subnet.develop.id
     nat       = true
