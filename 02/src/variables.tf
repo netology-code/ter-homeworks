1f3ef182fdb26993a78ccdbc91bd2390857e9d5f
dimka <ddd@ddd>
2024-01-13 14:26:43 +0300

    1

--- 02/src/variables.tf
+++ 02/src/variables.tf
@@ -36,6 +36,6 @@ variable "vpc_name" {
 
 variable "vms_ssh_root_key" {
   type        = string
-  default     = "<your_ssh_ed25519_key>"
+  default     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJXrG37ahpXCptSBiQG4ukIF6X+Xm6NyY0zlsUlXn0DX root@docker"
   description = "ssh-keygen -t ed25519"
 }
