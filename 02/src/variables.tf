07dfef7a5d97b3ff2d46d2de9b05b86e98a6f59c
dimka <ddd@ddd>
2024-01-14 14:53:43 +0300

    fix

--- 02/src/variables.tf
+++ 02/src/variables.tf
@@ -56,20 +56,20 @@ variable "test" {
   default = [
     {
       dev1 = [
-        "c",
-        "10.0.1.7",
+        "",
+        "",
       ]
     },
     {
       dev2 = [
-        "ssh -o 'StrictHostKeyChecking=no' ubuntu@84.252.140.88",
-        "10.0.2.29",
+        "",
+        "",
       ]
     },
     {
       prod1 = [
-        "ssh -o 'StrictHostKeyChecking=no' ubuntu@51.250.2.101",
-        "10.0.1.30",
+        "",
+        "",
       ]
     },
   ]
