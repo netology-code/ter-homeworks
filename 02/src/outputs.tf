d71f60e23a2d60bd3a29a1ff95d07ba2bee40fb6
dimka <ddd@ddd>
2024-01-14 15:31:45 +0300

    fix

--- 02/src/outputs.tf
+++ 02/src/outputs.tf
@@ -1,18 +1,18 @@
-output "instances_info" {
-  value = [
-    {
-      WEB = [
-        "external_ip: ${yandex_compute_instance.platform.network_interface[0].nat_ip_address}",
-        "instance_name: ${yandex_compute_instance.platform.name}",
-        "fqdn: ${yandex_compute_instance.platform.fqdn}"
-      ]
-    },
-    {
-      DB = [
-        "external_ip: ${yandex_compute_instance.platform-db.network_interface[0].nat_ip_address}",
-        "instance_name: ${yandex_compute_instance.platform-db.name}",
-        "fqdn: ${yandex_compute_instance.platform-db.fqdn}"
-      ]
-    }
-  ]
-}
+#output "instances_info" {
+#  value = [
+#    {
+#      WEB = [
+#        "external_ip: ${yandex_compute_instance.platform.network_interface[0].nat_ip_address}",
+#        "instance_name: ${yandex_compute_instance.platform.name}",
+#        "fqdn: ${yandex_compute_instance.platform.fqdn}"
+#      ]
+#    },
+#    {
+#      DB = [
+#        "external_ip: ${yandex_compute_instance.platform-db.network_interface[0].nat_ip_address}",
+#        "instance_name: ${yandex_compute_instance.platform-db.name}",
+#        "fqdn: ${yandex_compute_instance.platform-db.fqdn}"
+#      ]
+#    }
+#  ]
+#}
