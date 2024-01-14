53341e1b6432ddb7fe56d4f3995e42a654e047b1
dimka <ddd@ddd>
2024-01-14 15:59:52 +0300

    fix

--- 02/src/network_nat.tf
+++ 02/src/network_nat.tf
@@ -29,26 +29,26 @@ resource "yandex_vpc_route_table" "rt_web" {
 }
 
 #DB
-resource "yandex_vpc_subnet" "nat_db" {
-  folder_id      = var.folder_id
-  name           = var.vpc_name_2
-  v4_cidr_blocks = var.default_cidr
-  zone           = var.zone_b
-  network_id     = yandex_vpc_network.develop-2.id
-  route_table_id = yandex_vpc_route_table.rt_db.id
-}
-
-resource "yandex_vpc_gateway" "nat_gateway_db" {
-  name = var.test_nat_gateway_DB
-  shared_egress_gateway {}
-}
-
-resource "yandex_vpc_route_table" "rt_db" {
-  name       = var.route_table_name_DB
-  network_id = yandex_vpc_network.develop-2.id
-
-  static_route {
-    destination_prefix = "0.0.0.0/0"
-    gateway_id         = yandex_vpc_gateway.nat_gateway_db.id
-  }
-}
+#resource "yandex_vpc_subnet" "nat_db" {
+#  folder_id      = var.folder_id
+#  name           = var.vpc_name_2
+#  v4_cidr_blocks = var.default_cidr
+#  zone           = var.zone_b
+#  network_id     = yandex_vpc_network.develop-2.id
+#  route_table_id = yandex_vpc_route_table.rt_db.id
+#}
+#
+#resource "yandex_vpc_gateway" "nat_gateway_db" {
+#  name = var.test_nat_gateway_DB
+#  shared_egress_gateway {}
+#}
+#
+#resource "yandex_vpc_route_table" "rt_db" {
+#  name       = var.route_table_name_DB
+#  network_id = yandex_vpc_network.develop-2.id
+#
+#  static_route {
+#    destination_prefix = "0.0.0.0/0"
+#    gateway_id         = yandex_vpc_gateway.nat_gateway_db.id
+#  }
+#}
