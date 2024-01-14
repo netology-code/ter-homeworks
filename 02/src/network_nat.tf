2d74349e93f23fc5ed00da6f63e7a562366fdd4b
dimka <ddd@ddd>
2024-01-14 15:39:31 +0300

    fix

--- 02/src/network_nat.tf
+++ 02/src/network_nat.tf
@@ -1,14 +1,14 @@
-data "yandex_vpc_network" "net" {
-  folder_id = var.folder_id
-  name      = var.vpc_name
-}
+#data "yandex_vpc_network" "net" {
+#  folder_id = var.folder_id
+#  name      = var.vpc_name
+#}
 
 resource "yandex_vpc_subnet" "nat" {
   folder_id      = var.folder_id
   name           = var.vpc_name
   v4_cidr_blocks = var.default_cidr
   zone           = var.default_zone
-  network_id     = data.yandex_vpc_network.net.id
+  network_id     = yandex_vpc_network.develop.id
   route_table_id = yandex_vpc_route_table.rt.id
 }
 
@@ -19,7 +19,7 @@ resource "yandex_vpc_gateway" "nat_gateway" {
 
 resource "yandex_vpc_route_table" "rt" {
   name       = var.route_table_name
-  network_id = data.yandex_vpc_network.net.id
+  network_id = yandex_vpc_network.develop.id
 
   static_route {
     destination_prefix = "0.0.0.0/0"
