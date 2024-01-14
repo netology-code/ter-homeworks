1940737c3fdb1997fc379f8c040da71c2ee96cef
Alekc <alekc.genom@yandex.ru>
2024-01-14 17:32:48 +0000

    edit main.tf

--- 02/src/providers.tf
+++ 02/src/providers.tf
@@ -12,5 +12,5 @@ provider "yandex" {
   cloud_id                 = var.cloud_id
   folder_id                = var.folder_id
   zone                     = var.default_zone
-  service_account_key_file = file("~/.authorized_key.json")
+  service_account_key_file = file("~/authorized_key.json")
 }
