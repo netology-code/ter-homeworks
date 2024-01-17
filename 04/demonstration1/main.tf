59818b52ac8901becdbc1235a15705949c468522
Eugene Misyakov <udjin10@gmail.com>
2024-01-17 18:05:29 +0300

    some fixes

--- 04/demonstration1/main.tf
+++ 04/demonstration1/main.tf
@@ -1,21 +1,3 @@
-terraform {
-  required_providers {
-    yandex = {
-      source = "yandex-cloud/yandex"
-    }
-  }
-  required_version = ">=0.13"
-}
-
-provider "yandex" {
-  token     = var.token
-  cloud_id  = var.cloud_id
-  folder_id = var.folder_id
-  zone      = var.default_zone
-}
-
-
-
 #создаем облачную сеть
 resource "yandex_vpc_network" "develop" {
   name = "develop"
@@ -30,25 +12,25 @@ resource "yandex_vpc_subnet" "develop" {
 }
 
 module "test-vm" {
-  source          = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
-  env_name        = "develop"
-  network_id      = yandex_vpc_network.develop.id
-  subnet_zones    = ["ru-central1-a"]
-  subnet_ids      = [ yandex_vpc_subnet.develop.id ]
-  instance_name   = "web"
-  instance_count  = 2
-  image_family    = "ubuntu-2004-lts"
-  public_ip       = true
-  
+  source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
+  env_name       = "develop"
+  network_id     = yandex_vpc_network.develop.id
+  subnet_zones   = ["ru-central1-a"]
+  subnet_ids     = [yandex_vpc_subnet.develop.id]
+  instance_name  = "web"
+  instance_count = 2
+  image_family   = "ubuntu-2004-lts"
+  public_ip      = true
+
   metadata = {
-      user-data          = data.template_file.cloudinit.rendered #Для демонстрации №3
-      serial-port-enable = 1
+    user-data          = data.template_file.cloudinit.rendered #Для демонстрации №3
+    serial-port-enable = 1
   }
 
 }
 
 #Пример передачи cloud-config в ВМ для демонстрации №3
 data "template_file" "cloudinit" {
- template = file("./cloud-init.yml")
+  template = file("./cloud-init.yml")
 }
 
