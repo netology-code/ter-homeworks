73c0d7dbc00851646ddda70c146cdb77b6fb70b0
dimka <ddd@ddd>
2024-01-19 16:39:02 +0300

    fix

--- 03/src/count_vm.tf
+++ 03/src/count_vm.tf
@@ -1,15 +1,3 @@
-resource "yandex_vpc_network" "develop" {
-  name = var.vpc_name
-}
-
-#Resources sub_network without nat
-resource "yandex_vpc_subnet" "develop" {
-  name           = var.vpc_name
-  zone           = var.default_zone
-  network_id     = yandex_vpc_network.develop.id
-  v4_cidr_blocks = var.default_cidr
-}
-
 data "yandex_compute_image" "ubuntu" {
   family = var.vm_web_yandex_compute_image
 }
