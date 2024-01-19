8f4493190091223b44aeedf963eac0468a22c621
dimka <ddd@ddd>
2024-01-19 11:53:09 +0300

    fix

--- 03/src/providers.tf
+++ 03/src/providers.tf
@@ -8,8 +8,9 @@ terraform {
 }
 
 provider "yandex" {
-  token     = var.token
-  cloud_id  = var.cloud_id
-  folder_id = var.folder_id
-  zone      = var.default_zone
+  # token     = var.token
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+  zone                     = var.default_zone
+  service_account_key_file = file("authorized_key.json")
 }
