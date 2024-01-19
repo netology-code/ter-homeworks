225b5607c26fc713075e589fbc37096b3f109a6c
dimka <ddd@ddd>
2024-01-19 15:59:15 +0300

    fix

--- 03/src/variables.tf
+++ 03/src/variables.tf
@@ -1,16 +1,18 @@
 ###cloud vars
-variable "token" {
-  type        = string
-  description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
-}
+#variable "token" {
+#  type        = string
+#  description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
+#}
 
 variable "cloud_id" {
   type        = string
+  default     = ""
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
 }
 
 variable "folder_id" {
   type        = string
+  default     = ""
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
 }
 
