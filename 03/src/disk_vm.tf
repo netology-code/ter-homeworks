98a9a10fffedef5d7844535de58a304d0127f598
dimka <ddd@ddd>
2024-01-19 22:46:33 +0300

    fix

--- 03/src/disk_vm.tf
+++ 03/src/disk_vm.tf
@@ -1,8 +1,8 @@
-resource "yandex_compute_disk" "default" {
-  count = 3
-  name     = "${var.vm_and_disk.task_3.disk_name}-${count.index + 1}"
-  type     = var.vm_and_disk.task_3.disk_type
-  zone     = var.vm_and_disk.task_3.disk_zone
-  image_id = var.vm_and_disk.task_3.disk_image_id
-}
-
+#resource "yandex_compute_disk" "default" {
+#  count = 3
+#  name     = "${var.vm_and_disk.task_3.disk_name}-${count.index + 1}"
+#  type     = var.vm_and_disk.task_3.disk_type
+#  zone     = var.vm_and_disk.task_3.disk_zone
+#  image_id = var.vm_and_disk.task_3.disk_image_id
+#}
+#
