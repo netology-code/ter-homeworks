9a2169fe3e3c54101cdcab31e3b2762a26e018ee
dimka <ddd@ddd>
2024-01-19 16:52:07 +0300

    fix

--- 03/src/count_vm.tf
+++ 03/src/count_vm.tf
@@ -30,6 +30,7 @@ resource "yandex_compute_instance" "web" {
   network_interface {
     subnet_id = yandex_vpc_subnet.develop.id
     nat       = var.vm_web_.network_interface.nat
+    security_group_ids = yandex_vpc_security_group.example.id
   }
 
   metadata = {
