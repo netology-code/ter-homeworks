119c7c7d234b010186b87347225bd0461a47d7dd
dimka <ddd@ddd>
2024-01-20 18:27:19 +0300

    fix

--- 03/src/ansible.tf
+++ 03/src/ansible.tf
@@ -2,10 +2,7 @@ resource "local_file" "hosts_cfg" {
   content  = templatefile("${path.module}/hosts.tftpl", {
     webservers = yandex_compute_instance.web,
     databases  = yandex_compute_instance.db,
-    storage  = [
-      yandex_compute_instance.task_3.name,
-      yandex_compute_instance.task_3.network_interface[0].nat_ip_address
-    ]
+    storage  = yandex_compute_instance.task_3
   })
   filename = "${abspath(path.module)}/hosts.cfg"
 }
