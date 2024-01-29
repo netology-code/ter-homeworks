bcb97bffdd4a693f73f4fe538c2216f84d971ec7
Eugene Misyakov <udjin10@gmail.com>
2024-01-29 18:49:04 +0300

    improve 03 demo

--- 03/demo/main.tf
+++ 03/demo/main.tf
@@ -1,3 +1,5 @@
+#TODO: combine password, bcrypt, servername
+
 #создаем облачную сеть
 resource "yandex_vpc_network" "develop" {
   name = "develop"
@@ -18,10 +20,10 @@ data "yandex_compute_image" "ubuntu-2004-lts" {
 
 #создаем 2 идентичные ВМ
 resource "yandex_compute_instance" "example" {
+  count = 2
+
   name        = "netology-develop-platform-web-${count.index}"
   platform_id = "standard-v1"
-
-  count = 1
 
   resources {
     cores         = 2
@@ -51,7 +53,7 @@ resource "yandex_compute_instance" "example" {
 }
 
 
-resource "local_file" "hosts_cfg" {
+resource "local_file" "hosts_templatefile" {
   content = templatefile("${path.module}/hosts.tftpl",
 
   { webservers = yandex_compute_instance.example })
@@ -59,8 +61,46 @@ resource "local_file" "hosts_cfg" {
   filename = "${abspath(path.module)}/hosts.cfg"
 }
 
+resource "local_file" "hosts_for" {
+  content =  <<-EOT
+  %{if length(yandex_compute_instance.example) > 0}
+  [webservers]
+  %{endif}
+  %{for i in yandex_compute_instance.example }
+  ${i["name"]}   ansible_host=${i["network_interface"][0]["nat_ip_address"]}
+  %{endfor}
+  EOT
+  filename = "${abspath(path.module)}/for.cfg"
+
+}
+
+resource "random_password" "solo" {
+  length = 17
+#> type.random_password.solo  list(object)
+}
+
+resource "random_password" "count" {
+  count    = length([ for i in yandex_compute_instance.example: i])
+  length = 17
+#> type(random_password.count)  list(object)
+}
+
+
+resource "random_password" "each" {
+  for_each    = toset([for k, v in yandex_compute_instance.example : v.name ])
+  length = 17
+#> type(random_password.each) object(object)
+}
+
+variable "web_provision" {
+  type    = bool
+  default = true
+  description="ansible provision switch variable"
+}
+
 
 resource "null_resource" "web_hosts_provision" {
+  count = var.web_provision == true ? 1 : 0 #var.web_provision ? 1 : 0
   #Ждем создания инстанса
   depends_on = [yandex_compute_instance.example]
 
@@ -70,22 +110,36 @@ resource "null_resource" "web_hosts_provision" {
   }
 
   #Костыль!!! Даем ВМ 60 сек на первый запуск. Лучше выполнить это через wait_for port 22 на стороне ansible
-  # В случае использования cloud-init может потребоваться еще больше времени
-  provisioner "local-exec" {
-    command = "sleep 60"
-  }
+  # # В случае использования cloud-init может потребоваться еще больше времени
+  # provisioner "local-exec" {
+  #   command = "sleep 60"
+  # }
 
   #Запуск ansible-playbook
   provisioner "local-exec" {
-    command     = "export ANSIBLE_HOST_KEY_CHECKING=False; ansible-playbook -i ${abspath(path.module)}/hosts.cfg ${abspath(path.module)}/test.yml"
+    # without secrets
+    # command     = "export ANSIBLE_HOST_KEY_CHECKING=False; ansible-playbook -i ${abspath(path.module)}/hosts.cfg ${abspath(path.module)}/test.yml"
+    
+    #secrets pass
+    #> nonsensitive(jsonencode( {for k,v in random_password.each: k=>v.result}))
+    /*
+      "{\"netology-develop-platform-web-0\":\"u(qzeC#nKjp*wTOY\",\"netology-develop-platform-web-1\":\"=pA12\\u0026C2eCl[Oe$9\"}"
+    */
+    command     = "export ANSIBLE_HOST_KEY_CHECKING=False; ansible-playbook -i ${abspath(path.module)}/hosts.cfg ${abspath(path.module)}/test.yml --extra-vars '{\"secrets\": ${jsonencode( {for k,v in random_password.each: k=>v.result})} }'"
+
+    # for complex cases instead  --extra-vars "key=value", use --extra-vars "@some_file.json"
+
     on_failure  = continue #Продолжить выполнение terraform pipeline в случае ошибок
     environment = { ANSIBLE_HOST_KEY_CHECKING = "False" }
     #срабатывание триггера при изменении переменных
   }
   triggers = {
-    always_run        = "${timestamp()}"                         #всегда т.к. дата и время постоянно изменяются
+    # always_run        = "${timestamp()}"                         #всегда т.к. дата и время постоянно изменяются
     playbook_src_hash = file("${abspath(path.module)}/test.yml") # при изменении содержимого playbook файла
-    ssh_public_key    = var.public_key                           # при изменении переменной
+    ssh_public_key    = var.public_key                           # при изменении переменной with ssh
+    template_rendered = "${local_file.hosts_templatefile.content}" #при изменении inventory-template
+    password_change = jsonencode( {for k,v in random_password.each: k=>v.result})
+
   }
 
 }
--- 03/demo/variables.tf
+++ 03/demo/variables.tf
@@ -4,3 +4,5 @@ variable "public_key" {
   type    = string
   default = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIOERolx3KtJ5+3FufMC88MhcYISKmL28lYydyc4MGYV udjin@udjin-VirtualBox"
 }
+
+
