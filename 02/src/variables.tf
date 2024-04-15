bc3dee834d39e38a320f147657dba7436dad9e01
Alex <alexash1109@bk.ru>
2024-04-15 14:49:22 +0300

    for 1 task

--- 02/src/variables.tf
+++ 02/src/variables.tf
@@ -36,6 +36,6 @@ variable "vpc_name" {
 
 variable "vms_ssh_root_key" {
   type        = string
-  default     = "<your_ssh_ed25519_key>"
+  default     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOkSL7MyZ/ONOD1efFYMYB7QuQoTjXBYu0/rTD6Y6vi1 debian@debian"
   description = "ssh-keygen -t ed25519"
 }
