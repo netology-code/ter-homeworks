b80353b592a60e20c0a75f7c8354f42f2b3cb616
udjin <udjin10@gmail.com>
2024-05-31 18:58:56 +0300

    fix

--- 03/demo/main.tf
+++ 03/demo/main.tf
@@ -106,7 +106,9 @@ resource "null_resource" "web_hosts_provision" {
 
   #Добавление ПРИВАТНОГО ssh ключа в ssh-agent
   provisioner "local-exec" {
-    command = "cat ~/.ssh/id_ed25519 | ssh-add -"
+    command = "eval $(ssh-agent) && cat ~/.ssh/id_ed25519 | ssh-add -"
+    on_failure  = continue #Продолжить выполнение terraform pipeline в случае ошибок
+
   }
 
   #Костыль!!! Даем ВМ 60 сек на первый запуск. Лучше выполнить это через wait_for port 22 на стороне ansible
@@ -134,11 +136,11 @@ resource "null_resource" "web_hosts_provision" {
     #срабатывание триггера при изменении переменных
   }
   triggers = {
-    # always_run        = "${timestamp()}"                         #всегда т.к. дата и время постоянно изменяются
-    playbook_src_hash = file("${abspath(path.module)}/test.yml") # при изменении содержимого playbook файла
-    ssh_public_key    = var.public_key                           # при изменении переменной with ssh
-    template_rendered = "${local_file.hosts_templatefile.content}" #при изменении inventory-template
-    password_change = jsonencode( {for k,v in random_password.each: k=>v.result})
+    always_run        = "${timestamp()}"                         #всегда т.к. дата и время постоянно изменяются
+    # playbook_src_hash = file("${abspath(path.module)}/test.yml") # при изменении содержимого playbook файла
+    # ssh_public_key    = var.public_key                           # при изменении переменной with ssh
+    # template_rendered = "${local_file.hosts_templatefile.content}" #при изменении inventory-template
+    # password_change = jsonencode( {for k,v in random_password.each: k=>v.result})
 
   }
 
