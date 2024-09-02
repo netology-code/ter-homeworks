00193d49e84e3241c28eef194173aa316e26dfe7
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2024-09-02 17:27:46 +0300

    Update vms_platform.tf

--- 02/src/vms_platform.tf
+++ 02/src/vms_platform.tf
@@ -25,15 +25,25 @@ variable "default_zone-db" {
   description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
 }
 
-variable "default_cidr" {
+variable "default_cidr-web" {
+  type        = list(string)
+  default     = ["10.0.1.0/24"]
+  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+}
+variable "default_cidr-db" {
   type        = list(string)
   default     = ["10.0.1.0/24"]
   description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
 }
 
-variable "vpc_name" {
+variable "vpc_name-web" {
   type        = string
-  default     = "develop"
+  default     = "develop-web"
+  description = "VPC network & subnet name"
+}
+variable "vpc_name-db" {
+  type        = string
+  default     = "develop-db"
   description = "VPC network & subnet name"
 }
 
