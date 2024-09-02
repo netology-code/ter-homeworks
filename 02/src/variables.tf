26295560156f87bf67a870cd141cbc3525de34f2
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2024-09-02 12:51:54 +0300

    Update variables.tf

--- 02/src/variables.tf
+++ 02/src/variables.tf
@@ -36,6 +36,6 @@ variable "vpc_name" {
 
 variable "vms_ssh_root_key" {
   type        = string
-  default     = "1111"
+  default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDCpYiAOHRWfQngzfWfpjq8tIe+Czk08aER7ytsEgj/Dn9rSN1c32dSBp55UIJOHE9Sc6EHmIe/1FPqEzR1CYzcqIA1nk4DuBgtCwwEbUAqRBHQjKGAyp7n2Mtiyv82X5M12NDB6v2iHf47vEakFRK4zxAWU+yw1C8bvkIKTG5LTGmqRJy+rZmQRuzhoA+UAyg2CRnrosFRcu3sa/GV/atsNCv838i+HoPhTiPyRPA9rWxQr1fTVmqVWaswMylOBV1jvsHzICvdL71QIUkvi2Cq798/Zsi8tP6BrsB0snnIMuXAwjNPWaqIHU/JbSsNLSbc+sFSpUw1TQhsd6S2mr6Fz7xV356XBhL1JfEdPty+29O5x/tnSp28AtO4CAscK+6ea2arFjDiurhlGz1AUu2xALT5DbiiHouWIjdcnT+CLcMy6V0Nod2cDZ3apDb54IEleMFllYLe8jbbg+XUklQqqWEy0wN2WsHgo9kQG6uyUKbJrPVd53O5Gik/BnOWtSU="
   description = "ssh-keygen -t ed25519"
 }
