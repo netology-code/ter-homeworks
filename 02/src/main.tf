ed31ce6ed59f9b40e353afba8505d7c759666081
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2024-09-02 15:25:26 +0300

    Update main.tf

--- 02/src/main.tf
+++ 02/src/main.tf
@@ -8,10 +8,10 @@ resource "yandex_vpc_subnet" "develop" {
   v4_cidr_blocks = var.default_cidr
 }
 
-
 data "yandex_compute_image" "ubuntu" {
   family = var.vm_web_yandex_compute_image_family
 }
+
 resource "yandex_compute_instance" "platform" {
   name        = var.vm_web_yandex_compute_instance_name
   platform_id = var.vm_web_yandex_compute_instance_platform_id
@@ -32,10 +32,34 @@ resource "yandex_compute_instance" "platform" {
     subnet_id = yandex_vpc_subnet.develop.id
     nat       = true
   }
-
   metadata = {
     serial-port-enable = 1
     ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
   }
+}
 
+resource "yandex_compute_instance" "platform" {
+  name        = var.vm_db_yandex_compute_instance_name
+  platform_id = var.vm_db_yandex_compute_instance_platform_id
+  resources {
+    cores         = var.db_web_yandex_compute_instance_resources_cores
+    memory        = var.db_web_yandex_compute_instance_resources_memory
+    core_fraction = var.db_web_yandex_compute_instance_resources_core_fraction
+  }
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.image_id
+    }
+  }
+  scheduling_policy {
+    preemptible = true
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop.id
+    nat       = true
+  }
+  metadata = {
+    serial-port-enable = 1
+    ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
+  }
 }
