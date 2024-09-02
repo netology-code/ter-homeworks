c3be8f24c72ad9634a7cc1e92d6d15cb1e900ba4
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2024-09-02 17:20:52 +0300

    Update providers.tf

--- 02/src/providers.tf
+++ 02/src/providers.tf
@@ -11,6 +11,6 @@ provider "yandex" {
   # token     = var.token
   cloud_id                 = var.cloud_id
   folder_id                = var.folder_id
-  zone                     = var.default_zone
+  zone                     = var.default_zone-web
   service_account_key_file = file("~/key.json")
 }
