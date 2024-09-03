ed6bd676c0773d80b2ccafbdbd071b3efae6f8ac
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2024-09-03 10:09:50 +0300

    u

--- 02/src/outputs.tf
+++ 02/src/outputs.tf
@@ -1,5 +1,6 @@
 output "instance_name" { 
-  value = yandex_compute_instance.vm_*_yandex_compute_instance_name.name
+  value = yandex_compute_instance.vm_web_yandex_compute_instance_name.name
+  value = yandex_compute_instance.vm_db_yandex_compute_instance_name.name
 }
 
 output "fqdn" {
