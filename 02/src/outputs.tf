9636a36ae12d89a8149029f9c9891d1a79eada6a
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2024-09-03 15:45:49 +0300

    u

--- 02/src/outputs.tf
+++ 02/src/outputs.tf
@@ -1,11 +1,23 @@
-output "instance_name" { 
-  value = yandex_compute_instance.vm_*_yandex_compute_instance_name.name
+output "netology-develop-platform-web instance_name" { 
+  value = yandex_compute_instance.vm_web_yandex_compute_instance_name.name
 }
 
-output "fqdn" {
-  value = yandex_compute_instance.vm_*_yandex_compute_instance_name.fqdn
+output "netology-develop-platform-web fqdn" {
+  value = yandex_compute_instance.vm_web_yandex_compute_instance_name.fqdn
 }
 
-output "external_ip" {
-  value = yandex_compute_instance.vm_*_yandex_compute_instance_name.network_interface.0.nat_ip_address
+output "netology-develop-platform-web external_ip" {
+  value = yandex_compute_instance.vm_web_yandex_compute_instance_name.network_interface.0.nat_ip_address
+}
+
+output "netology-develop-platform-db instance_name" { 
+  value = yandex_compute_instance.vm_db_yandex_compute_instance_name.name
+}
+
+output "netology-develop-platform-db fqdn" {
+  value = yandex_compute_instance.vm_db_yandex_compute_instance_name.fqdn
+}
+
+output "netology-develop-platform-db external_ip" {
+  value = yandex_compute_instance.vm_db_yandex_compute_instance_name.network_interface.0.nat_ip_address
 }
