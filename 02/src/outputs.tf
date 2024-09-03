dff9ccea054c6110495e857597b634cc55fedd3c
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2024-09-03 18:20:42 +0300

    h

--- 02/src/outputs.tf
+++ 02/src/outputs.tf
@@ -1,7 +1,8 @@
 output "develop" { 
   value = [
-    { "Platform WEB instance_name: yandex_compute_instance.platform-web.name" }
+    { "Platform WEB instance_name: ${yandex_compute_instance.platform-web.name}" }
   ]
+}
 
 /*
 output "platform_web_fqdn" {
@@ -23,5 +24,3 @@ output "platform_db_fqdn" {
 output "platform_db_external_ip" {
   value = yandex_compute_instance.platform-db.network_interface.0.nat_ip_address
 */
-
-}
