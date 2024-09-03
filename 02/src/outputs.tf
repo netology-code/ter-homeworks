3f94bfcbba8be6055ee5fb768ee45459e80f55ba
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2024-09-03 15:46:33 +0300

    uu

--- 02/src/outputs.tf
+++ 02/src/outputs.tf
@@ -1,23 +1,23 @@
-output "netology-develop-platform-web instance_name" { 
+output "netology_develop_platform_web instance_name" { 
   value = yandex_compute_instance.vm_web_yandex_compute_instance_name.name
 }
 
-output "netology-develop-platform-web fqdn" {
+output "netology_develop_platform_web fqdn" {
   value = yandex_compute_instance.vm_web_yandex_compute_instance_name.fqdn
 }
 
-output "netology-develop-platform-web external_ip" {
+output "netology_develop_platform_web external_ip" {
   value = yandex_compute_instance.vm_web_yandex_compute_instance_name.network_interface.0.nat_ip_address
 }
 
-output "netology-develop-platform-db instance_name" { 
+output "netology_develop_platform_db instance_name" { 
   value = yandex_compute_instance.vm_db_yandex_compute_instance_name.name
 }
 
-output "netology-develop-platform-db fqdn" {
+output "netology_develop_platform_db fqdn" {
   value = yandex_compute_instance.vm_db_yandex_compute_instance_name.fqdn
 }
 
-output "netology-develop-platform-db external_ip" {
+output "netology_develop_platform_db external_ip" {
   value = yandex_compute_instance.vm_db_yandex_compute_instance_name.network_interface.0.nat_ip_address
 }
