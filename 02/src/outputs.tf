50d905d4ef6b6f69e4de43885ba4012c813e1a39
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2024-09-03 18:35:00 +0300

    Update

--- 02/src/outputs.tf
+++ 02/src/outputs.tf
@@ -1,7 +1,8 @@
 output "develop" { 
   value = [
-    { external_name_web = [yandex_compute_instance.platform-web.name, yandex_compute_instance.platform-db.name] },
-    { external_name_db  = yandex_compute_instance.platform-db.name }
+    { instance_name = [yandex_compute_instance.platform-web.name, yandex_compute_instance.platform-db.name] },
+    { external_ip = [yandex_compute_instance.platform-web.network_interface.0.nat_ip_addres, yandex_compute_instance.platform-db.network_interface.0.nat_ip_addres] },
+    { fqdn = [yandex_compute_instance.platform-web.fqdn, yandex_compute_instance.platform-db.fqdn] }
   ]
 }
 
