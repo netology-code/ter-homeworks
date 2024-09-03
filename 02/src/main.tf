4c531195ae2c66be16d16bb9d5d323e3ca6874da
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2024-09-03 18:03:43 +0300

    d

--- 02/src/main.tf
+++ 02/src/main.tf
@@ -41,8 +41,8 @@ resource "yandex_compute_instance" "platform-web" {
     nat       = true
   }
   metadata = {
-    serial-port-enable = 1
-    ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
+    serial-port-enable = var.metadata.vm.serial-port-enable
+    ssh-keys           = var.metadata.vm.ssh-keys
   }
 }
 
