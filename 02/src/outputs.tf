efe2dc2589f808705f1faeab9ae1a7dad7ba3d7f
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2024-09-03 09:25:42 +0300

    Update outputs.tf

--- 02/src/outputs.tf
+++ 02/src/outputs.tf
@@ -1,11 +1,11 @@
 output "instance_name" { 
-  value = yandex_compute_instance.vm.*.name
+  value = yandex_compute_instance.*.name
 }
 
 output "fqdn" {
-  value = yandex_compute_instance.vm.*.hostname
+  value = yandex_compute_instance.*.hostname
 }
 
 output "external_ip" {
-  value = yandex_compute_instance.vm.*.network_interface.0.nat_ip_address
+  value = yandex_compute_instance.*.network_interface.0.nat_ip_address
 }
