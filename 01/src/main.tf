38e7e9abd918a44d8c48dd279e076227a3f152b2
gaidar.vu <gaidar.vu@gmail.com>
2024-10-31 14:34:51 +0300

    name

--- 01/src/main.tf
+++ 01/src/main.tf
@@ -27,7 +27,8 @@ resource "docker_image" "nginx" {
 
 resource "docker_container" "nginx" {
   image = docker_image.nginx.image_id
-  name  = "example_${random_password.random_string.result}"
+  name  = "hello_world"
+  # name  = "example_${random_password.random_string.result}"
 
   ports {
     internal = 80
