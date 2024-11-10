90dd0584f5e1feb3215af9fb820901a9e6447efd
gaidar.vu <gaidar.vu@gmail.com>
2024-11-10 22:46:06 +0300

    yc4

--- 04/src/main.tf
+++ 04/src/main.tf
@@ -8,3 +8,54 @@ resource "yandex_vpc_subnet" "develop" {
   v4_cidr_blocks = var.default_cidr
 }
 
+module "marketing_vm" {
+  source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
+  env_name       = "develop" 
+  network_id     = yandex_vpc_network.develop.id
+  subnet_zones   = ["ru-central1-a", "ru-central1-b"]
+  subnet_ids     = [yandex_vpc_subnet.develop.id]
+  instance_name  = "webs"
+  image_family   = "ubuntu-2004-lts"
+  public_ip      = true
+
+  labels = { 
+    owner= "gaidar_vu",
+    project = "marketing"
+     }
+
+  metadata = {
+    user-data          = data.template_file.cloudinit.rendered
+    serial-port-enable = 1
+  }
+
+}
+
+module "analytics_vm" {
+  source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
+  env_name       = "stage"
+  network_id     = yandex_vpc_network.develop.id
+  subnet_zones   = ["ru-central1-a"]
+  subnet_ids     = [yandex_vpc_subnet.develop.id]
+  instance_name  = "web-stage"
+  instance_count = 1
+  image_family   = "ubuntu-2004-lts"
+  public_ip      = true
+
+  labels = { 
+    owner= "gaidar_vu",
+    project = "analytics"
+     }
+
+  metadata = {
+    user-data          = data.template_file.cloudinit.rendered
+    serial-port-enable = 1
+  }
+
+}
+
+data "template_file" "cloudinit" {
+  template = file("./cloud-init.yml")
+  vars = {
+    ssh_authorized_keys = var.vms_ssh_root_key
+  }
+}
--- 04/src/variables.tf
+++ 04/src/variables.tf
@@ -19,6 +19,7 @@ variable "default_zone" {
   default     = "ru-central1-a"
   description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
 }
+
 variable "default_cidr" {
   type        = list(string)
   default     = ["10.0.1.0/24"]
@@ -35,7 +36,6 @@ variable "vpc_name" {
 
 variable "vms_ssh_root_key" {
   type        = string
-  default     = "your_ssh_ed25519_key"
   description = "ssh-keygen -t ed25519"
 }
 
@@ -52,6 +52,3 @@ variable "vm_db_name" {
   default     = "netology-develop-platform-db"
   description = "example vm_db_ prefix"
 }
-
-
-
