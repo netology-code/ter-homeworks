52b281bf8d6639084000bfb547b1810303bc9c63
oleg <a112-03@yandex.ru>
2025-05-29 06:10:25 -0700

    v07

--- 02/src/main.tf
+++ 02/src/main.tf
@@ -42,6 +42,57 @@ resource "yandex_compute_instance" "platform" {
   metadata = {
     serial-port-enable = 1
     ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
+   #ssh-keygen -t ed25519  На ed25519 переконфигурирую после сдачи ДЗ, а то половину уже сделал.
+  }
+
+}
+#################
+
+resource "yandex_vpc_network" "develop-b" {
+  name = var.vpc_name
+}
+resource "yandex_vpc_subnet" "develop-b" {
+  name           = var.vpc_name
+  zone           = var.default_zone_b
+  network_id     = yandex_vpc_network.develop.id
+  v4_cidr_blocks = var.default_cidr
+}
+
+
+#data "yandex_compute_image" "ubuntu" {
+#  family = "${var.vm_web_ubuntu-2004-lts}" 
+#}
+
+resource "yandex_compute_instance" "platform-db" {
+  name        = "${var.vm_db_netology-develop-platform-db}"
+  #name        = "netology-develop-platform-db"
+  platform_id = "${var.vm_db_standard-v3}"
+  #platform_id = "standard-v3"
+  resources {
+    cores         = "${var.vm_db_cores}"
+  #  cores         = 2
+    memory        = "${var.vm_db_memory}"
+  #  memory        = 2
+    core_fraction = "${var.vm_db_core_fraction}"
+  #  core_fraction = 20
+  }
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.image_id
+    }
+  }
+  scheduling_policy {
+    preemptible = true
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop.id
+    nat       = true
+  }
+
+  metadata = {
+    serial-port-enable = 1
+    ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
+   #ssh-keygen -t ed25519  На ed25519 переконфигурирую после сдачи ДЗ, а то половину уже сделал.
   }
 
 }
---
+++ 02/src/vms_platform.tf
@@ -0,0 +1,166 @@
+###cloud vars
+
+
+variable "cloud_id" {
+  type        = string
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
+}
+
+variable "folder_id" {
+  type        = string
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
+}
+
+variable "default_zone" {
+  type        = string
+  default     = "ru-central1-b"
+  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+}
+variable "default_cidr" {
+  type        = list(string)
+  default     = ["10.0.1.0/24"]
+  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+}
+
+variable "vpc_name" {
+  type        = string
+  default     = "develop"
+  description = "VPC network & subnet name"
+}
+
+
+###ssh vars
+
+variable "vms_ssh_root_key" {
+  type        = string
+  default     = "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAvr32tDFrCTKSsMbxVR4qhjZgfZhOSNDA4upGuhmIz1eBC/u04FTIEkV/wYr+etL02VYhUqyVZfGZ6okz0bQauegG7KDe/lJtdsox8abYQvSi8MzQnTL0jg5ug+8tj2nVJw8Dj84Q4/v5glIIxWjsiukizgSR4zEijGbre42BAdL95CB9mhxYksDuV7ygf7fQk4m6anirqtQsShyEcGBTDuc9EbJrhfQzmGWTwwm7YK7B3VXXUZD18kcg8xir8gNNck9QqfYifZokXDYD107u6sGjDFjyN3cXGvdoK8HMrCo+uUcB989QA1I35932kiayIVfAZStsvMbmOA1+6Kgw6Q== rsa 2048-20250512"
+  description = "ssh-keygen -t ed25519"
+}
+
+
+
+
+###my vars
+variable "vm_web_ubuntu-2004-lts" {
+  type        = string
+  default     = "ubuntu-2004-lts"
+  description = "ubuntu-2004-lts"
+}
+
+variable "vm_web_netology-develop-platform-web" {
+  type        = string
+  default     = "netology-develop-platform-web"
+  description = "netology-develop-platform-web"
+}
+
+variable "vm_web_standard-v3" {
+  type        = string
+  default     = "standard-v3"
+  description = "standard-v3"
+}
+
+variable "vm_web_cores" {
+  type        = number
+  default     = "2"
+  description = "cores"
+}
+
+variable "vm_web_memory" {
+  type        = number
+  default     = "1"
+  description = "vm_web_memory"
+}
+
+variable "vm_web_core_fraction" {
+  type        = number
+  default     = "20"
+  description = "core_fraction"
+}
+####
+
+
+
+##cloud vars
+
+
+variable "cloud_id" {
+  type        = string
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
+}
+
+variable "folder_id" {
+  type        = string
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
+}
+
+variable "default_zone_b" {
+  type        = string
+  default     = "ru-central1-b"
+  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+}
+variable "default_cidr" {
+  type        = list(string)
+  default     = ["10.0.1.0/24"]
+  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+}
+
+variable "vpc_name" {
+  type        = string
+  default     = "develop"
+  description = "VPC network & subnet name"
+}
+
+
+###ssh vars
+
+variable "vms_ssh_root_key" {
+  type        = string
+  default     = "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAvr32tDFrCTKSsMbxVR4qhjZgfZhOSNDA4upGuhmIz1eBC/u04FTIEkV/wYr+etL02VYhUqyVZfGZ6okz0bQauegG7KDe/lJtdsox8abYQvSi8MzQnTL0jg5ug+8tj2nVJw8Dj84Q4/v5glIIxWjsiukizgSR4zEijGbre42BAdL95CB9mhxYksDuV7ygf7fQk4m6anirqtQsShyEcGBTDuc9EbJrhfQzmGWTwwm7YK7B3VXXUZD18kcg8xir8gNNck9QqfYifZokXDYD107u6sGjDFjyN3cXGvdoK8HMrCo+uUcB989QA1I35932kiayIVfAZStsvMbmOA1+6Kgw6Q== rsa 2048-20250512"
+  description = "ssh-keygen -t ed25519"
+}
+
+
+
+
+
+
+
+
+
+
+
+variable "vm_db_ubuntu-2004-lts" {
+  type        = string
+  default     = "ubuntu-2004-lts"
+  description = "ubuntu-2004-lts"
+}
+
+variable "vm_db_netology-develop-platform-db" {
+  type        = string
+  default     = "netology-develop-platform-db"
+  description = "netology-develop-platform-db"
+}
+
+variable "vm_db_standard-v3" {
+  type        = string
+  default     = "standard-v3"
+  description = "standard-v3"
+}
+
+variable "vm_db_cores" {
+  type        = number
+  default     = "2"
+  description = "cores"
+}
+
+variable "vm_db_memory" {
+  type        = number
+  default     = "2"
+  description = "vm_db_memory"
+}
+
+variable "vm_db_core_fraction" {
+  type        = number
+  default     = "20"
+  description = "core_fraction"
+}
