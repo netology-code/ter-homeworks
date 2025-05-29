2724241778d8d91657275c9a13453c0c0b01ebb9
oleg <a112-03@yandex.ru>
2025-05-29 13:07:52 -0700

    v13

---
+++ 02/src/locals.tf
@@ -0,0 +1,61 @@
+#variable "bad_example" { default = "${ var.env }–${ var.project }"}
+
+variable "role" {
+  type = map(object({
+    name = string
+    number  = number
+  }))
+  default = {
+    "name-db1" = {
+      name = "db"
+      number  = 01
+    },
+    "name-web2" = {
+      name = "web"
+      number  = 02
+    }
+  }
+}
+
+locals {
+name-db1 = "netology-develop-platform-${var.role.name-db1.name}${var.role.name-db1.number}"
+name-web1 = "netology-develop-platform-${var.role.name-web2.name}${var.role.name-web2.number}"
+}
+
+
+variable "vms_resources"  {
+  type = map(object({
+#  type = list(object({
+    instance_name   = string
+    cores = number
+    memory = number
+    core_fraction = number
+  }))
+default = {
+    "resources-web1" = {
+    instance_name = "web1"
+    cores = 2
+    memory = 1
+    core_fraction = 20
+    },
+    "resources-db1" =  {
+    instance_name = "db1"
+    cores = 2
+    memory = 2
+    core_fraction = 20
+    }
+  }
+}
+
+
+
+
+
+#locals {
+#name = "${ var.env }–${ var.project }"
+#test = "123"
+#}
+
+#variable "bad_example" { default = "${ var.env }–${ var.project }"}
+
+#variable  netology-develop-platform-${ var.role }
--- 02/src/main.tf
+++ 02/src/main.tf
@@ -3,26 +3,25 @@ resource "yandex_vpc_network" "develop" {
 }
 
 resource "yandex_vpc_subnet" "develop" {
-  name           = var.vpc_name
-  zone           = var.default_zone
+  name           = "${var.vpc_name}"
+  zone           = "${var.default_zone}"
   network_id     = yandex_vpc_network.develop.id
-  v4_cidr_blocks = var.default_cidr
+  v4_cidr_blocks = "${var.default_cidr}"
 }
 
 resource "yandex_vpc_subnet" "develop-b" {
-  name           = var.vpc_name-b
-  zone           = var.default_zone-b
+  name           = "${var.vpc_name-b}"
+  zone           = "${var.default_zone-b}"
   network_id     = yandex_vpc_network.develop.id
-  v4_cidr_blocks = var.default_cidr-b
+  v4_cidr_blocks = "${var.default_cidr-b}"
 }
 
 data "yandex_compute_image" "ubuntu" {
   family = "${var.vm_web_ubuntu-2004-lts}" 
-#  family = "ubuntu-2004-lts"
 }
 
 resource "yandex_compute_instance" "platform" {
-  name        = "${var.vm_web_netology-develop-platform-web}"
+  name        = "${local.name-web1}"
   platform_id = "${var.vm_web_standard-v3}"
   resources {
     cores         = "${var.vm_web_cores}"
@@ -52,7 +51,8 @@ resource "yandex_compute_instance" "platform" {
 ###########
 
 resource "yandex_compute_instance" "platform2" {
-  name        = "${var.vm_db_netology-develop-platform-db}"
+#  name        = "${var.vm_db_netology-develop-platform-db}"
+  name        = "${local.name-db1}"
   platform_id = "${var.vm_db_standard-v3}"
   zone        = "${var.default_zone-b}"   #var.default_zone-b
   resources {
