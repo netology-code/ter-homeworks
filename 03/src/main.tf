9e27a362ee2ce7e406e8d93692c0aee266d99b2e
oleg <a112-03@yandex.ru>
2025-06-02 07:21:12 -0700

    v3-2

--- 03/src/main.tf
+++ 03/src/main.tf
@@ -1,11 +1,17 @@
 resource "yandex_vpc_network" "develop" {
   name = var.vpc_name
+#  default_security_group_id = "enplpecm6j76s7feq4s5" 
+#  security_group_id = "enplpecm6j76s7feq4s5" 
+#  security_group_id = "example_dynamic"  # Указываем групп безопасности  
+#network_id = yandex_vpc_network.develop.id
+#  security_group_id = "var.yandex_vpc_security_group"
 }
 resource "yandex_vpc_subnet" "develop" {
   name           = var.vpc_name
   zone           = var.default_zone
   network_id     = yandex_vpc_network.develop.id
   v4_cidr_blocks = var.default_cidr
+# #security_group_id = "enplpecm6j76s7feq4s5"  # Указываем групп безопасности  
 }
 
 
