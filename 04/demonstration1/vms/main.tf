009f6eeaeed8578f682a1a3963fbd26ca87eb67f
oleg <a112-03@yandex.ru>
2025-06-05 03:43:56 -0700

    v4-1

--- 04/demonstration1/vms/main.tf
+++ 04/demonstration1/vms/main.tf
@@ -21,19 +21,20 @@ resource "yandex_vpc_subnet" "develop_b" {
 
 module "test-vm" {
   source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
-  env_name       = "develop" 
+  #env_name       = "develop" 
   network_id     = yandex_vpc_network.develop.id
   subnet_zones   = ["ru-central1-a","ru-central1-b"]
   subnet_ids     = [yandex_vpc_subnet.develop_a.id,yandex_vpc_subnet.develop_b.id]
-  instance_name  = "webs"
-  instance_count = 2
+  instance_name  = "marketing"
+  instance_count = 1
   image_family   = "ubuntu-2004-lts"
   public_ip      = true
+  
 
-  labels = { 
-    owner= "i.ivanov",
-    project = "accounting"
-     }
+   labels = { 
+  #   owner= "i.ivanov",
+     project = "marketing"
+      }
 
   metadata = {
     user-data          = data.template_file.cloudinit.rendered #Для демонстрации №3
@@ -44,14 +45,20 @@ module "test-vm" {
 
 module "example-vm" {
   source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
-  env_name       = "stage"
+  #env_name       = "stage"
   network_id     = yandex_vpc_network.develop.id
   subnet_zones   = ["ru-central1-a"]
   subnet_ids     = [yandex_vpc_subnet.develop_a.id]
-  instance_name  = "web-stage"
+  instance_name  = "analytics"
   instance_count = 1
   image_family   = "ubuntu-2004-lts"
   public_ip      = true
+
+
+   labels = { 
+  #   owner= "i.ivanov",
+     project = "analytics"
+      }
 
   metadata = {
     user-data          = data.template_file.cloudinit.rendered #Для демонстрации №3
@@ -60,8 +67,11 @@ module "example-vm" {
 
 }
 
-#Пример передачи cloud-config в ВМ для демонстрации №3
+#Пример передачи cloud-config в ВМ.(передали путь к yml файлу и переменную!_ssh_public_key)
 data "template_file" "cloudinit" {
   template = file("./cloud-init.yml")
+   vars = {
+     ssh_public_key = var.ssh_public_key
+   }
 }
 
--- 04/demonstration1/vms/providers.tf
+++ 04/demonstration1/vms/providers.tf
@@ -4,13 +4,21 @@ terraform {
       source = "yandex-cloud/yandex"
     }
   }
-  required_version = "~>1.8.4"
+  required_version = ">1.8.4"
 }
 
+# provider "yandex" {
+#   # token                    = "do not use!!!"
+#   cloud_id                 = "b1gn3ndpua1j6jaabf79"
+#   folder_id                = "b1gfu61oc15cb99nqmfe"
+#   service_account_key_file = file("~/.authorized_key.json")
+#   zone                     = "ru-central1-a" #(Optional) 
+# }
+
 provider "yandex" {
-  # token                    = "do not use!!!"
-  cloud_id                 = "b1gn3ndpua1j6jaabf79"
-  folder_id                = "b1gfu61oc15cb99nqmfe"
-  service_account_key_file = file("~/.authorized_key.json")
-  zone                     = "ru-central1-a" #(Optional) 
+#  token     = var.token # del
+  cloud_id  = var.cloud_id
+  folder_id = var.folder_id
+  zone      = var.default_zone
+  service_account_key_file = file("~/.authorized_key.json") #add
 }
