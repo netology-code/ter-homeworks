70cc33bcd60db590c6c1947b272be5ad3d280a67
oleg <a112-03@yandex.ru>
2025-06-10 04:54:43 -0700

    v5-5

--- 05/terraform-hotfix/remote_state_outputs.tf
+++
@@ -1,4 +0,0 @@
-output "out" {
-
-    value=concat(module.test-vm.fqdn , module.example-vm.fqdn)
-}
--- 05/terraform-hotfix/vpc-dev/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "network_id" {
-  value = yandex_vpc_network.develop.id
-}
-
-output "subnet_id" {
-    value = yandex_vpc_subnet.develop.id
-}
