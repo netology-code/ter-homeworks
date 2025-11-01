30d842b5134f4d13b203fdfa277410a81df3ebfa
emisyakov <udjin10@gmail.com>
2025-11-01 14:49:45 +0300

    fix 5

--- 05/demo/providers.tf
+++ 05/demo/providers.tf
@@ -28,11 +28,11 @@ terraform {
     # Не требует отдельной базы данных (DynamoDB/YDB)!
     use_lockfile = true
 
-    skip_region_validation      = true
     skip_credentials_validation = true
-    skip_requesting_account_id  = true # Необходимая опция Terraform для версии 1.6.1 и старше.
-    skip_s3_checksum            = true # Необходимая опция при описании бэкенда для Terraform версии 1.6.3 и старше.
-
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+    
     endpoints = {
       s3 = "https://storage.yandexcloud.net"
     }
