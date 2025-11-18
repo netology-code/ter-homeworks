69cf509719e99af25ebe9383a1b07fd5da45f565
sapr797 <unisapr797@gmail.com>
2025-11-18 04:46:06 +0300

    Update ansible.tf

--- 03/src/ansible.tf
+++ 03/src/ansible.tf
@@ -1,37 +1,29 @@
-# Inventory шаблон для Ansible
-data "template_file" "inventory" {
-  template = file("${path.module}/inventory.tpl")
-  
-  vars = {
-    web_instances = jsonencode([
+# Динамический inventory для Ansible
+resource "local_file" "ansible_inventory" {
+  content = templatefile("inventory.tpl", {
+    # Группа web - обрабатывает любое количество ВМ (2, 999, etc.)
+    web_instances = [
       for vm in yandex_compute_instance.web : {
         name = vm.name
-        id   = vm.id
-        fqdn = vm.fqdn
         ip   = vm.network_interface.0.nat_ip_address
       }
-    ])
-    database_instances = jsonencode([
+    ]
+    
+    # Группа db - обрабатывает любое количество ВМ  
+    db_instances = [
       for vm_key, vm in yandex_compute_instance.database_vm : {
         name = vm.name
-        id   = vm.id
-        fqdn = vm.fqdn
         ip   = vm.network_interface.0.nat_ip_address
       }
-    ])
-    storage_instances = jsonencode([
+    ]
+    
+    # Группа storage - обрабатывает одну или несколько ВМ
+    storage_instances = [
       {
         name = yandex_compute_instance.storage.name
-        id   = yandex_compute_instance.storage.id
-        fqdn = yandex_compute_instance.storage.fqdn
         ip   = yandex_compute_instance.storage.network_interface.0.nat_ip_address
       }
-    ])
-  }
-}
-
-# Создание inventory файла
-resource "local_file" "ansible_inventory" {
-  content  = data.template_file.inventory.rendered
+    ]
+  })
   filename = "${path.module}/inventory.ini"
 }
