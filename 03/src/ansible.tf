473425e9b5a5a3cb1c32727fb191b5b5aceb48b3
sapr797 <unisapr797@gmail.com>
2025-11-18 04:53:46 +0300

    Update ansible.tf

--- 03/src/ansible.tf
+++ 03/src/ansible.tf
@@ -1,27 +1,24 @@
-# Динамический inventory для Ansible
 resource "local_file" "ansible_inventory" {
   content = templatefile("inventory.tpl", {
-    # Группа web - обрабатывает любое количество ВМ (2, 999, etc.)
     web_instances = [
       for vm in yandex_compute_instance.web : {
         name = vm.name
         ip   = vm.network_interface.0.nat_ip_address
+        fqdn = vm.fqdn # Полное доменное имя ВМ
       }
     ]
-    
-    # Группа db - обрабатывает любое количество ВМ  
     db_instances = [
       for vm_key, vm in yandex_compute_instance.database_vm : {
         name = vm.name
         ip   = vm.network_interface.0.nat_ip_address
+        fqdn = vm.fqdn
       }
     ]
-    
-    # Группа storage - обрабатывает одну или несколько ВМ
     storage_instances = [
       {
         name = yandex_compute_instance.storage.name
         ip   = yandex_compute_instance.storage.network_interface.0.nat_ip_address
+        fqdn = yandex_compute_instance.storage.fqdn
       }
     ]
   })
