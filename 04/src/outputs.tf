4b5243e43dcb664c46c46f97aa41c60ed8a8672c
sapr797 <unisapr797@gmail.com>
2025-11-21 00:47:48 +0300

    Update outputs.tf

--- 04/src/outputs.tf
+++ 04/src/outputs.tf
@@ -1,20 +1,20 @@
 output "marketing_vm_info" {
   description = "Information about marketing VM"
   value = {
-    name        = module.marketing_vm.vm_name
-    external_ip = module.marketing_vm.external_ip
-    internal_ip = module.marketing_vm.internal_ip
-    labels      = module.marketing_vm.labels
+    name        = yandex_compute_instance.marketing_vm.name
+    external_ip = yandex_compute_instance.marketing_vm.network_interface.0.nat_ip_address
+    internal_ip = yandex_compute_instance.marketing_vm.network_interface.0.ip_address
+    labels      = yandex_compute_instance.marketing_vm.labels
   }
 }
 
 output "analytics_vm_info" {
   description = "Information about analytics VM"
   value = {
-    name        = module.analytics_vm.vm_name
-    external_ip = module.analytics_vm.external_ip
-    internal_ip = module.analytics_vm.internal_ip
-    labels      = module.analytics_vm.labels
+    name        = yandex_compute_instance.analytics_vm.name
+    external_ip = yandex_compute_instance.analytics_vm.network_interface.0.nat_ip_address
+    internal_ip = yandex_compute_instance.analytics_vm.network_interface.0.ip_address
+    labels      = yandex_compute_instance.analytics_vm.labels
   }
 }
 
@@ -30,7 +30,8 @@ output "vpc_network_info" {
 output "ssh_connection_commands" {
   description = "SSH connection commands for VMs"
   value = {
-    marketing = "ssh ubuntu@${module.marketing_vm.external_ip}"
-    analytics = "ssh ubuntu@${module.analytics_vm.external_ip}"
+    marketing = "ssh ubuntu@${yandex_compute_instance.marketing_vm.network_interface.0.nat_ip_address}"
+    analytics = "ssh ubuntu@${yandex_compute_instance.analytics_vm.network_interface.0.nat_ip_address}"
   }
+  sensitive = false
 }
