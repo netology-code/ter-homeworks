9ce0bf3bab74817d2e381fa2a4de79e63aa55793
sapr797 <unisapr797@gmail.com>
2025-11-23 12:11:15 +0300

    Update variables.tf
    Задание 4

--- 04/src/variables.tf
+++ 04/src/variables.tf
@@ -1,53 +1,20 @@
-###cloud vars
-variable "token" {
+variable "yc_token" {
   type        = string
-  description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
+  description = "Yandex Cloud OAuth token"
 }
 
-variable "cloud_id" {
+variable "yc_cloud_id" {
   type        = string
-  description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
+  description = "Yandex Cloud ID"
 }
 
-variable "folder_id" {
+variable "yc_folder_id" {
   type        = string
-  description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
+  description = "Yandex Cloud Folder ID"
 }
-
-variable "default_zone" {
-  type        = string
-  default     = "ru-central1-a"
-  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
-}
-
-variable "default_cidr" {
-  type        = list(string)
-  default     = ["10.0.1.0/24"]
-  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
-}
-
-variable "vpc_name" {
-  type        = string
-  default     = "develop"
-  description = "VPC network&subnet name"
-}
-
-###common vars
 
 variable "vms_ssh_root_key" {
   type        = string
-  description = "ssh-keygen -t ed25519"
-}
-
-### VM variables
-variable "vm_web_name" {
-  type        = string
-  default     = "netology-develop-platform-web"
-  description = "example vm_web_ prefix"
-}
-
-variable "vm_db_name" {
-  type        = string
-  default     = "netology-develop-platform-db"
-  description = "example vm_db_ prefix"
+  description = "SSH public key for VMs"
+  default     = ""
 }
