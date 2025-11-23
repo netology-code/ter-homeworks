6d83bd34e3941544a90d1284a400dcc7e26c9ef0
sapr797 <unisapr797@gmail.com>
2025-11-23 12:49:58 +0300

    Update main.tf
    Задание 5.1

--- 04/src/main.tf
+++ 04/src/main.tf
@@ -1,91 +1,45 @@
-terraform {
-  required_providers {
-    yandex = {
-      source = "yandex-cloud/yandex"
-    }
-  }
-}
-
-provider "yandex" {
-  token     = var.yc_token
-  cloud_id  = var.yc_cloud_id
-  folder_id = var.yc_folder_id
-  zone      = "ru-central1-a"
-}
-
-# Production VPC с подсетями во всех зонах
-module "vpc_prod" {
-  source   = "./modules/vpc"
-  env_name = "production"
-
-  subnets = [
-    { zone = "ru-central1-a", cidr = "10.0.1.0/24" },
-    { zone = "ru-central1-b", cidr = "10.0.2.0/24" },
-    { zone = "ru-central1-c", cidr = "10.0.3.0/24" },
-  ]
-}
-
-# Development VPC с одной подсетью
-module "vpc_dev" {
-  source   = "./modules/vpc"
-  env_name = "develop"
-
-  subnets = [
-    { zone = "ru-central1-a", cidr = "10.10.1.0/24" },
-  ]
-}
-
-# Виртуальные машины используют подсети из production VPC
-resource "yandex_compute_instance" "marketing_vm" {
-  name        = "marketing-vm"
-  platform_id = "standard-v3"
-  zone        = "ru-central1-a"
+resource "yandex_mdb_mysql_cluster" "mysql_cluster" {
+  name        = var.cluster_name
+  environment = var.environment
+  network_id  = var.network_id
+  version     = var.mysql_version
 
   resources {
-    cores  = 2
-    memory = 2
+    resource_preset_id = var.resource_preset_id
+    disk_type_id       = var.disk_type_id
+    disk_size          = var.disk_size
   }
 
-  boot_disk {
-    initialize_params {
-      image_id = "fd8vmcue7aajpmeo39kk"
-      size     = 20
+  # Динамическое создание хостов в зависимости от HA режима
+  dynamic "host" {
+    for_each = var.ha ? range(var.host_count) : range(1)
+    
+    content {
+      zone      = var.zones[host.key % length(var.zones)]
+      subnet_id = var.subnet_ids[host.key % length(var.subnet_ids)]
     }
   }
 
-  network_interface {
-    subnet_id = module.vpc_prod.subnet_ids["ru-central1-a"]
-    nat       = true
+  database {
+    name = "default_db"
   }
 
-  metadata = {
-    ssh-keys = "ubuntu:${file("~/.ssh/terraform_rsa.pub")}"
-  }
-}
-
-resource "yandex_compute_instance" "analytics_vm" {
-  name        = "analytics-vm"
-  platform_id = "standard-v3"
-  zone        = "ru-central1-a"
-
-  resources {
-    cores  = 2
-    memory = 2
-  }
-
-  boot_disk {
-    initialize_params {
-      image_id = "fd8vmcue7aajpmeo39kk"
-      size     = 20
+  user {
+    name     = var.username
+    password = var.password
+    permission {
+      database_name = "default_db"
+      roles         = ["ALL"]
     }
   }
 
-  network_interface {
-    subnet_id = module.vpc_prod.subnet_ids["ru-central1-a"]
-    nat       = true
+  # Настройки HA
+  backup_window_start {
+    hours   = 23
+    minutes = 59
   }
 
-  metadata = {
-    ssh-keys = "ubuntu:${file("~/.ssh/terraform_rsa.pub")}"
+  maintenance_window {
+    type = "ANYTIME"
   }
 }
