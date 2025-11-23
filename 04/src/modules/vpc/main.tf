11c5d26a3d81428c9fbb7703e942749ce320a34d
sapr797 <unisapr797@gmail.com>
2025-11-23 06:39:35 +0300

    Update main.tf
    Задание 2.1

--- 04/src/modules/vpc/main.tf
+++ 04/src/modules/vpc/main.tf
@@ -8,6 +8,30 @@ terraform {
     }
   }
 }
+
+variable "network_name" {
+  description = "Name of the VPC network"
+  type        = string
+  default     = "vpc-network"
+}
+
+variable "subnet_name" {
+  description = "Name of the subnet"
+  type        = string
+  default     = "vpc-subnet"
+}
+
+variable "zone" {
+  description = "Availability zone for the subnet"
+  type        = string
+}
+
+variable "cidr_blocks" {
+  description = "CIDR block for the subnet"
+  type        = string
+  default     = "192.168.10.0/24"
+}
+
 resource "yandex_vpc_network" "network" {
   name = var.network_name
 }
