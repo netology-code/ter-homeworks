d1133aae73a09d1d1f3df6c93042865d90a08aaf
sapr797 <unisapr797@gmail.com>
2025-11-23 09:50:03 +0300

    Update outputs.tf
    Задание 4

--- 04/src/modules/vpc/outputs.tf
+++ 04/src/modules/vpc/outputs.tf
@@ -1,54 +1,20 @@
-# modules/vpc/outputs.tf
-
 output "network_id" {
-  description = "ID of the created VPC network"
-  value       = yandex_vpc_network.network.id
-}
-
-output "subnet_id" {
-  description = "ID of the created subnet"
-  value       = yandex_vpc_subnet.subnet.id
+  value = yandex_vpc_network.network.id
 }
 
 output "network_name" {
-  description = "Name of the created VPC network"
-  value       = yandex_vpc_network.network.name
+  value = yandex_vpc_network.network.name
 }
 
-output "subnet_name" {
-  description = "Name of the created subnet"
-  value       = yandex_vpc_subnet.subnet.name
+output "subnet_ids" {
+  value = { for idx, subnet in yandex_vpc_subnet.subnets : var.subnets[idx].zone => subnet.id }
 }
 
-output "subnet_cidr_blocks" {
-  description = "CIDR blocks of the created subnet"
-  value       = yandex_vpc_subnet.subnet.v4_cidr_blocks
-}
-
-output "zone" {
-  description = "Availability zone of the subnet"
-  value       = yandex_vpc_subnet.subnet.zone
-}
-
-# Полная информация о подсети
-output "subnet_info" {
-  description = "Complete information about the subnet"
-  value = {
-    id           = yandex_vpc_subnet.subnet.id
-    name         = yandex_vpc_subnet.subnet.name
-    zone         = yandex_vpc_subnet.subnet.zone
-    network_id   = yandex_vpc_subnet.subnet.network_id
-    v4_cidr_blocks = yandex_vpc_subnet.subnet.v4_cidr_blocks
-    created_at   = yandex_vpc_subnet.subnet.created_at
-  }
-}
-
-# Полная информация о сети
-output "network_info" {
-  description = "Complete information about the network"
-  value = {
-    id      = yandex_vpc_network.network.id
-    name    = yandex_vpc_network.network.name
-    created_at = yandex_vpc_network.network.created_at
-  }
+output "subnets" {
+  value = { for idx, subnet in yandex_vpc_subnet.subnets : var.subnets[idx].zone => {
+    id             = subnet.id
+    name           = subnet.name
+    zone           = subnet.zone
+    v4_cidr_blocks = subnet.v4_cidr_blocks
+  } }
 }
