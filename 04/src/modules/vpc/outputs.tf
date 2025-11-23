8a193d31ed976c04d2df52f143c62984a453d0fb
sapr797 <unisapr797@gmail.com>
2025-11-23 07:59:59 +0300

    Update outputs.tf
    задание 2.3

--- 04/src/modules/vpc/outputs.tf
+++ 04/src/modules/vpc/outputs.tf
@@ -15,12 +15,40 @@ output "network_name" {
   value       = yandex_vpc_network.network.name
 }
 
-output "zone" {
-  description = "Availability zone of the subnet"
-  value       = yandex_vpc_subnet.subnet.zone
+output "subnet_name" {
+  description = "Name of the created subnet"
+  value       = yandex_vpc_subnet.subnet.name
 }
 
 output "subnet_cidr_blocks" {
   description = "CIDR blocks of the created subnet"
   value       = yandex_vpc_subnet.subnet.v4_cidr_blocks
+}
+
+output "zone" {
+  description = "Availability zone of the subnet"
+  value       = yandex_vpc_subnet.subnet.zone
+}
+
+# Полная информация о подсети
+output "subnet_info" {
+  description = "Complete information about the subnet"
+  value = {
+    id           = yandex_vpc_subnet.subnet.id
+    name         = yandex_vpc_subnet.subnet.name
+    zone         = yandex_vpc_subnet.subnet.zone
+    network_id   = yandex_vpc_subnet.subnet.network_id
+    v4_cidr_blocks = yandex_vpc_subnet.subnet.v4_cidr_blocks
+    created_at   = yandex_vpc_subnet.subnet.created_at
+  }
+}
+
+# Полная информация о сети
+output "network_info" {
+  description = "Complete information about the network"
+  value = {
+    id      = yandex_vpc_network.network.id
+    name    = yandex_vpc_network.network.name
+    created_at = yandex_vpc_network.network.created_at
+  }
 }
