f4c6f279a1da67a018a5164d4407dced5dc90e2a
sapr797 <unisapr797@gmail.com>
2025-11-23 13:21:46 +0300

    Update variables.tf
    Задание 5.2

--- 04/src/modules/mysql-cluster/variables.tf
+++ 04/src/modules/mysql-cluster/variables.tf
@@ -1,74 +1,44 @@
-variable "cluster_name" {
+variable "cluster_id" {
   type        = string
-  description = "Name of the MySQL cluster"
+  description = "ID of the existing MySQL cluster"
 }
 
-variable "network_id" {
+variable "database_name" {
   type        = string
-  description = "Network ID where the cluster will be created"
-}
-
-variable "ha" {
-  type        = bool
-  description = "High Availability mode (true for multi-host, false for single host)"
-  default     = false
-}
-
-variable "host_count" {
-  type        = number
-  description = "Number of hosts in the cluster"
-  default     = 2
-}
-
-variable "environment" {
-  type        = string
-  description = "Environment (production, staging, development)"
-  default     = "production"
-}
-
-variable "mysql_version" {
-  type        = string
-  description = "MySQL version"
-  default     = "8.0"
-}
-
-variable "resource_preset_id" {
-  type        = string
-  description = "Resource preset ID for hosts"
-  default     = "s2.micro"
-}
-
-variable "disk_size" {
-  type        = number
-  description = "Disk size in GB"
-  default     = 20
-}
-
-variable "disk_type_id" {
-  type        = string
-  description = "Disk type ID"
-  default     = "network-ssd"
-}
-
-variable "zones" {
-  type        = list(string)
-  description = "List of availability zones for hosts"
-  default     = ["ru-central1-a", "ru-central1-b", "ru-central1-c"]
-}
-
-variable "subnet_ids" {
-  type        = list(string)
-  description = "List of subnet IDs for hosts (must match zones)"
+  description = "Name of the database to create"
 }
 
 variable "username" {
   type        = string
-  description = "MySQL admin username"
-  default     = "admin"
+  description = "Name of the user to create"
 }
 
 variable "password" {
   type        = string
-  description = "MySQL admin password"
+  description = "Password for the user"
   sensitive   = true
+}
+
+variable "permissions" {
+  type = list(object({
+    database_name = string
+    roles         = list(string)
+  }))
+  description = "List of database permissions for the user"
+  default = [{
+    database_name = "default_db"
+    roles         = ["ALL"]
+  }]
+}
+
+variable "connection_limit" {
+  type        = number
+  description = "Maximum number of connections for the user"
+  default     = 10
+}
+
+variable "global_permissions" {
+  type        = list(string)
+  description = "Global permissions for the user"
+  default     = []
 }
