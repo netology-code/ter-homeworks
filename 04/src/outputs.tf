d716d14d1f9bcaac0211f902e21a894255756e5d
sapr797 <unisapr797@gmail.com>
2025-11-23 12:12:04 +0300

    Update outputs.tf
    Задание 4

--- 04/src/outputs.tf
+++ 04/src/outputs.tf
@@ -1,50 +1,35 @@
-# outputs.tf
+output "prod_network" {
+  value = module.vpc_prod.network_name
+}
+
+output "prod_subnets" {
+  value = module.vpc_prod.subnet_ids
+}
+
+output "dev_network" {
+  value = module.vpc_dev.network_name
+}
+
+output "dev_subnets" {
+  value = module.vpc_dev.subnet_ids
+}
+
+# Outputs для виртуальных машин
+output "analytics_vm_ip" {
+  value = yandex_compute_instance.analytics_vm.network_interface[0].nat_ip_address
+}
 
 output "marketing_vm_ip" {
-  description = "External IP address of the marketing VM"
-  value       = module.marketing_vm.external_ip
+  value = yandex_compute_instance.marketing_vm.network_interface[0].nat_ip_address
 }
 
 output "marketing_vm_internal_ip" {
-  description = "Internal IP address of the marketing VM"
-  value       = module.marketing_vm.internal_ip
-}
-
-output "analytics_vm_ip" {
-  description = "External IP address of the analytics VM"
-  value       = yandex_compute_instance.analytics_vm.network_interface[0].nat_ip_address
-}
-
-output "marketing_vm_info" {
-  description = "Complete information about marketing VM"
-  value = {
-    name        = module.marketing_vm.name
-    external_ip = module.marketing_vm.external_ip
-    internal_ip = module.marketing_vm.internal_ip
-  }
-}
-
-output "vpc_network_info" {
-  description = "Information about VPC network"
-  value = {
-    network_id    = module.vpc.network_id
-    subnet_id     = module.vpc.subnet_id
-    network_name  = module.vpc.network_name
-    subnet_name   = module.vpc.subnet_name
-    cidr_blocks   = module.vpc.subnet_cidr_blocks
-    zone          = module.vpc.zone
-  }
-}
-
-output "subnet_complete_info" {
-  description = "Complete information about subnet from VPC module"
-  value       = module.vpc.subnet_info
+  value = yandex_compute_instance.marketing_vm.network_interface[0].ip_address
 }
 
 output "ssh_connection_commands" {
-  description = "SSH connection commands for VMs"
   value = {
-    marketing = "ssh -i ~/.ssh/terraform_rsa ubuntu@${module.marketing_vm.external_ip}"
     analytics = "ssh -i ~/.ssh/terraform_rsa ubuntu@${yandex_compute_instance.analytics_vm.network_interface[0].nat_ip_address}"
+    marketing = "ssh -i ~/.ssh/terraform_rsa ubuntu@${yandex_compute_instance.marketing_vm.network_interface[0].nat_ip_address}"
   }
 }
