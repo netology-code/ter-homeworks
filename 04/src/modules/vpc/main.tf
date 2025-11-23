89061c98f2852799ab713cda3d5d4521b0f290d8
sapr797 <unisapr797@gmail.com>
2025-11-23 09:37:16 +0300

    Update main.tf
    Задание 4

--- 04/src/modules/vpc/main.tf
+++ 04/src/modules/vpc/main.tf
@@ -36,9 +36,12 @@ resource "yandex_vpc_network" "network" {
   name = var.network_name
 }
 
-resource "yandex_vpc_subnet" "subnet" {
-  name           = var.subnet_name
-  zone           = var.zone
+# Создание подсетей в разных зонах доступности
+resource "yandex_vpc_subnet" "subnets" {
+  count = length(var.subnets)
+
+  name           = "${var.env_name}-subnet-${var.subnets[count.index].zone}"
+  zone           = var.subnets[count.index].zone
   network_id     = yandex_vpc_network.network.id
-  v4_cidr_blocks = [var.cidr_blocks]
+  v4_cidr_blocks = [var.subnets[count.index].cidr]
 }
