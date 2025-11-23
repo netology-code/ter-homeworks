3fa2d1e373fb9606035a55fdbd5cded15becdce7
sapr797 <unisapr797@gmail.com>
2025-11-23 10:03:18 +0300

    Update main.tf
    Задание 4

--- 04/src/main.tf
+++ 04/src/main.tf
@@ -27,6 +27,28 @@ module "vpc" {
   ]
 }
 
+# Модуль VPC для production во всех зонах
+module "vpc_prod" {
+  source   = "./modules/vpc"
+  env_name = "production"
+
+  subnets = [
+    { zone = "ru-central1-a", cidr = "10.0.1.0/24" },
+    { zone = "ru-central1-b", cidr = "10.0.2.0/24" },
+    { zone = "ru-central1-c", cidr = "10.0.3.0/24" },
+  ]
+}
+
+# Модуль VPC для development в одной зоне
+module "vpc_dev" {
+  source   = "./modules/vpc"
+  env_name = "develop"
+
+  subnets = [
+    { zone = "ru-central1-a", cidr = "10.10.1.0/24" },
+  ]
+}
+
   # Передаем параметры сети
   #network_name = "production-network"
   #subnet_name  = "production-subnet"
