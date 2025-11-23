ebe72eea83b725832b4fd99035199c8d0f3dbdc8
sapr797 <unisapr797@gmail.com>
2025-11-23 11:51:50 +0300

    Update main.tf
    Задание 4

--- 04/src/modules/vpc/main.tf
+++ 04/src/modules/vpc/main.tf
@@ -1,42 +1,7 @@
-# modules/vpc/main.tf
-
-terraform {
-  required_providers {
-    yandex = {
-      source  = "yandex-cloud/yandex"
-      version = ">= 0.171.0"
-    }
-  }
-}
-
-variable "network_name" {
-  description = "Name of the VPC network"
-  type        = string
-  default     = "vpc-network"
-}
-
-variable "subnet_name" {
-  description = "Name of the subnet"
-  type        = string
-  default     = "vpc-subnet"
-}
-
-variable "zone" {
-  description = "Availability zone for the subnet"
-  type        = string
-}
-
-variable "cidr_blocks" {
-  description = "CIDR block for the subnet"
-  type        = string
-  default     = "192.168.10.0/24"
-}
-
 resource "yandex_vpc_network" "network" {
-  name = var.network_name
+  name = "${var.env_name}-network"
 }
 
-# Создание подсетей в разных зонах доступности
 resource "yandex_vpc_subnet" "subnets" {
   count = length(var.subnets)
 
