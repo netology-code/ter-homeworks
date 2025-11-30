bc06c3e091900debe522f2ca8c08a94fb41155c0
sapr797 <unisapr797@gmail.com>
2025-11-30 09:23:00 +0000

    Complete task 4: Add IP address validation with comprehensive testing
    - Add regex validation for ip_address variable
    - Add validation for ip_list using alltrue() and regex
    - Test both valid and invalid IP addresses
    - Demonstrate validation working in isolated environment
    - All requirements for task 4 completed

--- 04/src/variables.tf
+++ 04/src/variables.tf
@@ -53,5 +53,29 @@ variable "vpc_name" {
   #description = "example vm_db_ prefix"
 #}
 
+# Валидация для одиночного IP-адреса
+variable "ip_address" {
+  type        = string
+  description = "ip-адрес"
+  default     = "192.168.0.1"
 
+  validation {
+    condition = can(regex("^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$", var.ip_address))
+    error_message = "Invalid IP address format."
+  }
+}
+
+# Валидация для списка IP-адресов
+variable "ip_list" {
+  type        = list(string)
+  description = "список ip-адресов"
+  default     = ["192.168.0.1", "1.1.1.1", "127.0.0.1"]
+
+  validation {
+    condition = alltrue([
+      for ip in var.ip_list : can(regex("^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$", ip))
+    ])
+    error_message = "One or more IP addresses in the list are invalid."
+  }
+}
 
