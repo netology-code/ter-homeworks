02388824ba63b3caca2ac9d03ec006633e9ffcfb
Eugene M. <70715492+udjin10@users.noreply.github.com>
2026-01-07 13:45:23 +0300

    Update providers.tf

--- 05/demo/providers.tf
+++ 05/demo/providers.tf
@@ -16,7 +16,6 @@ terraform {
   backend "s3" {
     
     shared_credentials_files = ["~/.aws/credentials"]
-    shared_config_files      = ["~/.aws/config"]
     profile                  = "default"
     region                   = "ru-central1"
 
