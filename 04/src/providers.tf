2b473237741be6aa33407799d3a061eb184ec6ee
Zolotkoff <-email@-.com>
2026-07-14 20:24:31 +0300

    terraform-05: configure S3 remote state backend with use_lockfile

--- 04/src/providers.tf
+++ 04/src/providers.tf
@@ -5,6 +5,23 @@ terraform {
     }
   }
   required_version = "~>1.12.0"
+
+  backend "s3" {
+    bucket = "zolotkoff-tf-state"
+    key    = "terraform.tfstate"
+    region = "ru-central1"
+
+    use_lockfile = true
+
+    endpoints = {
+      s3 = "https://storage.yandexcloud.net"
+    }
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+  }
 }
 
 provider "yandex" {
